3039d68ca275d87d56dba57437dfe847cbd815fb
Sujitha-D-2002 <sujitha.dhamotharan@cdw.com>
2023-03-28 19:08:34 +0530

    Exercise-5-Design Patterns

--- Exercise-5/VideoPlayerDemo.java
+++ Exercise-5/VideoPlayerDemo.java
@@ -1,44 +1,47 @@
 /*
  * Bridge Pattern
  */
-interface VideoPlatform{
+interface VideoPlatform {
 	public String playOn();
 }
-class Netflix implements VideoPlatform{
+
+class Netflix implements VideoPlatform {
 	@Override
 	public String playOn() {
 		return "Netflix";
 	}
 }
-class HotStar implements VideoPlatform{
+
+class HotStar implements VideoPlatform {
 	@Override
 	public String playOn() {
 		return "Hotstar";
-	}	
-}
-
-
-abstract class Video{
-	VideoPlatform platform;
-	public Video(VideoPlatform plat) {
-		platform=plat;
 	}
-	abstract public void play(); 
 }
 
+abstract class Video {
+	VideoPlatform platform;
 
-class Movie extends Video{
-	public Movie(VideoPlatform plat){
+	public Video(VideoPlatform plat) {
+		platform = plat;
+	}
+
+	abstract public void play();
+}
+
+class Movie extends Video {
+	public Movie(VideoPlatform plat) {
 		super(plat);
 	}
-	
+
 	public void play() {
-		System.out.println("Play video in "+platform.playOn());
+		System.out.println("Play video in " + platform.playOn());
 	}
 }
+
 public class VideoPlayerDemo {
 	public static void main(String[] args) {
-		Video video=new Movie(new Netflix());
+		Video video = new Movie(new Netflix());
 		video.play();
 
 	}
--- Exercise-5/WordDocument.java
+++ Exercise-5/WordDocument.java
@@ -1,3 +1,4 @@
+
 /**
  * Momento Pattern
  */
@@ -7,33 +8,33 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
+public class WordDocument {
 
-public class WordDocument{
-	
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		/**
 		 * Trying to create a file word.dat and write content in that file
 		 */
-		Word word=new Word();
-		System.out.println("The content of word Document:"+word.content);
-		ObjectOutputStream oos=new ObjectOutputStream(new FileOutputStream("word.dat"));
-		word.content="Word Document Content updated";
+		Word word = new Word();
+		System.out.println("The content of word Document:" + word.content);
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("word.dat"));
+		word.content = "Word Document Content updated";
 		oos.writeObject(word);
 		/**
 		 * Trying to update content in that file(word.dat)
 		 */
-		word.content="Word Document Content updated";
-		System.out.println("\nThe content of word Document:"+word.content);
+		word.content = "Word Document Content updated";
+		System.out.println("\nThe content of word Document:" + word.content);
 		/**
 		 * Trying to restore the content(like undo operation)
 		 */
 		System.out.println("\nAfter Restoring");
-		ObjectInputStream ois=new ObjectInputStream(new FileInputStream("word.dat"));
-		word=(Word)ois.readObject();
-		System.out.println("Undo content..:"+word.content);
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("word.dat"));
+		word = (Word) ois.readObject();
+		System.out.println("Undo content..:" + word.content);
 	}
 
 }
-class Word implements Serializable{
-	String content="Word Document content....";
+
+class Word implements Serializable {
+	String content = "Word Document content....";
 }
